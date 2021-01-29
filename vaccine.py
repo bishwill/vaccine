2840b3b6878b55fc94ceb7fabe3275b9d080788a
Will Bishop <will.bishop9427@gmail.com>
2021-01-29 17:09:20 +0000

    version_1.1

--- vaccine.py
+++ vaccine.py
@@ -18,6 +18,7 @@ def get_data():
     global daily_vaccines
     global data
     global time_of_update
+    global date_of_update
     r = get('https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newPeopleVaccinatedFirstDoseByPublishDate%22:%22newPeopleVaccinatedFirstDoseByPublishDate%22,%22newPeopleVaccinatedSecondDoseByPublishDate%22:%22newPeopleVaccinatedSecondDoseByPublishDate%22,%22cumPeopleVaccinatedFirstDoseByPublishDate%22:%22cumPeopleVaccinatedFirstDoseByPublishDate%22,%22cumPeopleVaccinatedSecondDoseByPublishDate%22:%22cumPeopleVaccinatedSecondDoseByPublishDate%22%7D&format=json')
     latest_data = r.json()['data'][0]
     if latest_data != data:
@@ -29,9 +30,9 @@ def get_data():
 
 def predictor(cum_vaccinated, daily_vaccines):
     time = datetime.now().strftime("%H:%M:%S")
-    date = str(date.today())[-2:]
+    date_now = str(date.today())[-2:]
     time_diff = ((60*60*int(time[0:2])) + (60*int(time[3:5])) + (int(time[6:8])))
-    if date == date_of_update:
+    if int(date_now) == int(date_of_update) + 1:
         date_diff = 0
     else:
         date_diff = 1
@@ -55,9 +56,6 @@ device = ssd1322(serial, rotate = 2)
 while True:
     sleep(1)
     display()
-
-
-    
     if datetime.now().strftime("%M") == '00':
         get_data()
         sleep(60)
